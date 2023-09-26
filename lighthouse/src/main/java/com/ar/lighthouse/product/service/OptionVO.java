9d431df0fbb8d9994f83c6e8d3fcda63e72f43bd
seojiwon99 <83777261+seojiwon99@users.noreply.github.com>
2023-09-26 15:15:18 +0900

    option

--- lighthouse/src/main/java/com/ar/lighthouse/product/service/OptionVO.java
+++ lighthouse/src/main/java/com/ar/lighthouse/product/service/OptionVO.java
@@ -1,12 +1,16 @@
 package com.ar.lighthouse.product.service;
 
+import java.util.List;
+
 import lombok.Data;
 
 @Data
 public class OptionVO {
+
+	private int optionCode;
+	String productCode;
+	String optionName;
+	String optionValue;
 	
-	private Integer optionDetailCode;
-	private String productCode;
-	private String optionName;
-	private String optionValue;
+	List<OptionDetailVO> detailVO;
 }
