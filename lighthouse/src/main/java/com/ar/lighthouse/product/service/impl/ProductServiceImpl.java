b2021173c748dc9a810d09cb7e3a54973536b8d5
seojiwon99 <83777261+seojiwon99@users.noreply.github.com>
2023-10-02 11:52:09 +0900

    괄호체크

--- lighthouse/src/main/java/com/ar/lighthouse/product/service/impl/ProductServiceImpl.java
+++ lighthouse/src/main/java/com/ar/lighthouse/product/service/impl/ProductServiceImpl.java
@@ -257,7 +257,8 @@ public class ProductServiceImpl implements ProductService{
 	@Override
 	public int updateCancelList(CancelVO cancelVO) {
 		return productMapper.updateCancelList(cancelVO);
-
+	}
+	
 	// 석연 - 취소 완료 시 Y로 상태 변경
     @Override
 	public int editCancelOk(String cancelCode) {
