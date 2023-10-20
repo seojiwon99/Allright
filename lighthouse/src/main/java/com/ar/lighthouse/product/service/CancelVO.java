bf852160db8441504b388210e6ab85765599787d
Son <seokson1@nave.com>
2023-10-20 10:26:24 +0900

    주문 환불 시 재고 복원 기능 구현

--- lighthouse/src/main/java/com/ar/lighthouse/product/service/CancelVO.java
+++ lighthouse/src/main/java/com/ar/lighthouse/product/service/CancelVO.java
@@ -10,45 +10,51 @@ import lombok.Data;
 
 @Data
 public class CancelVO {
-	String cancelCode;
-	long orderDetailCode;
-	String cancelReason;
-	
-	@DateTimeFormat(pattern="yyyy-MM-dd")
-	Date cancelRegdate;
-	
-	@DateTimeFormat(pattern="yyyy-MM-dd")
-	Date cancelRetractDate;
-	
-	String cancelStatus;
-	
-	@DateTimeFormat(pattern="yyyy-MM-dd")
-	Date cancelDate;
-	String cancelRejectionReason;
+   String cancelCode;
+   long orderDetailCode;
+   String cancelReason;
+   
+   @DateTimeFormat(pattern="yyyy-MM-dd")
+   Date cancelRegdate;
+   
+   @DateTimeFormat(pattern="yyyy-MM-dd")
+   Date cancelRetractDate;
+   
+   String cancelStatus;
+   
+   @DateTimeFormat(pattern="yyyy-MM-dd")
+   Date cancelDate;
+   String cancelRejectionReason;
 
-	
-	@DateTimeFormat(pattern="yyyy-MM-dd")
-	Date chargeDate;
-	
-	
-	   // 멤버 테이블
-	private String memberId;
-	private String memberName;
-	private int memberTel;
-	   
-	   //상품 테이블 조인
-	   private String orderStatus;
-	   private String productName;
-	   private String productCode;
-	   
-//	   orderVO
-	   int orderCode;
-	   
-	//   ajax넘어오는값
-	   String searchValue;
-	   String searchKey;
-	   @DateTimeFormat(pattern="yyyy-MM-dd")
-	   Date fromDate;
-	   @DateTimeFormat(pattern="yyyy-MM-dd")
-	   Date toDate;
+   
+   @DateTimeFormat(pattern="yyyy-MM-dd")
+   Date chargeDate;
+   
+   
+      // 멤버 테이블
+   private String memberId;
+   private String memberName;
+   private int memberTel;
+      
+      //상품 테이블 조인
+      private String orderStatus;
+      private String productName;
+      private String productCode;
+      
+      int optionDetailCode;
+      
+//      orderVO
+      int orderCode;
+      
+//      orderDetail
+      int orderCnt;
+      
+      
+   //   ajax넘어오는값
+      String searchValue;
+      String searchKey;
+      @DateTimeFormat(pattern="yyyy-MM-dd")
+      Date fromDate;
+      @DateTimeFormat(pattern="yyyy-MM-dd")
+      Date toDate;
 }
